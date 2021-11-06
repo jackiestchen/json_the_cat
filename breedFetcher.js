2fdabb45b1178721043e383164bc4e20f28d149b
jackiestchen <jackiestchen@gmail.com>
2021-11-06 17:52:31 -0600

    Implement test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,57 @@
 const request = require('request');
 
-const breed = process.argv[2];
+// const breed = process.argv[2];
 
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+// const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(URL, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    // console.log(response);
-    const data = JSON.parse(body);
-    if (!data[0]) {
-      console.log("No breed found!");
+// request(URL, (error, response, body) => {
+//   if (error) {
+//     return error;
+//   } else {
+//     // console.log(response);
+//     const data = JSON.parse(body);
+//     if (!data[0]) {
+//       console.log("No breed found!");
+//     } else {
+//       // console.log(data[0].description);
+//       return data[0].description;
+//     }
+//   }
+
+// });
+
+const fetchBreedDescription = (breedName, callback) => {
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  // callback(catAPIRequest(URL));
+  let err = null;
+  let desc = null;
+
+  request(URL, (error, response, body) => {
+    // console.log(error);
+    // console.log(body);
+    if (error) {
+      err = error;
+      
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      if (!data[0]) {
+        err = "Invalid breed input";
+      } else {        
+        desc = data[0].description;
+      }
     }
-  }
+    callback(err, desc);
+  });
 
-});
+};
+
+
+// fetchBreedDescription("siam", (error, desc) => {
+//   if (error) {
+//     console.log('Error fetch details: ', error);
+//   } else {
+//     console.log(desc);
+//   }
+// })
+
+module.exports = {fetchBreedDescription};
